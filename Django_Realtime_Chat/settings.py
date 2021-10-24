7d0d98fcc1d4f28779c7ab725def660f9143c7bc
sahaib9747 <pro.sahaib9747@gmail.com>
2021-10-25 02:45:03 +0600

    database added

--- Django_Realtime_Chat/settings.py
+++ Django_Realtime_Chat/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'Django_Realtime_Chat.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_realtime_chat',
+        'USER': 'admin',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': 3306
     }
 }
 
--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,18 @@
+from datetime import datetime
+
 from django.db import models
+from django.db.models.fields import DateTimeField, TextField
 
 # Create your models here.
+class Room(models.Model):
+    name = models.TextField(blank=False)
+    password = models.TextField(blank=False)
+    
+    def __str__(self) -> str:
+        return self.name
+
+class Message(models.Model):
+    room = TextField(blank=False)
+    user = TextField(blank=False)
+    message = TextField(blank=False)
+    date = DateTimeField(default=datetime.now())
